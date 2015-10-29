5a574703deb2fd0b0ca8eb8feb9e22621ece016b
张全鹏 <274430533@qq.com>
2015-10-29 22:32:26 +0800

    fix account

--- VPN-Air/VPNAccount.m
+++ VPN-Air/VPNAccount.m
@@ -8,15 +8,6 @@
 
 #import "VPNAccount.h"
 
-//VPN
-/*************************************************/
-#define kVPNName @"zqpmaster"
-//#define kServerAddress @"66.155.104.74"
-#define kServerAddress @"192.161.61.132"
-#define kVPNPassword @"99464189"
-#define kVPNSharePSK @"vpn.psk"
-/*************************************************/
-
 @implementation VPNAccount
 
 + (instancetype)shareManager
